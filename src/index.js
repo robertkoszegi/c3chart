1967f01f8c362ac8c4f54ce16bb2f11e53fe76e2
robertkoszegi <robertkoszegi@radiostuntmen.com>
2024-12-17 22:34:16 -0500

    set up the initial chart

--- src/index.js
+++ src/index.js
@@ -1,5 +1,59 @@
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
+import c3 from 'c3';
+
+const data = [
+	{
+	  "month": "Jan",
+	  "Apples": 327,
+	  "Peaches": 437,
+	  "Pears": 111
+	},
+	{
+	  "month": "Feb",
+	  "Apples": 555,
+	  "Peaches": 454,
+	  "Pears": 222
+	},
+	{
+	  "month": "Mar",
+	  "Apples": 242,
+	  "Peaches": 864,
+	  "Pears": 343
+	},
+	{
+	  "month": "Apr",
+	  "Apples": 222,
+	  "Peaches": 454,
+	  "Pears": 434
+	}
+]
+
+const options = {
+	bindto: '#chart',
+	axis: {
+		x: { type: 'category' },
+		y: {}
+	},
+	size: {
+		height: 480,
+		width: 960
+	
+	},
+	data: {
+		labels: true,
+		type: "bar",
+		json: data,
+		keys: {
+			x: 'month',
+			value: ['Apples', 'Peaches', 'Pears']
+		}
+		
+	},
+
+	padding: {
+		bottom: 0
+	
+	}
+	
+}
+
+const chart = c3.generate(options);
