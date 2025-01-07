47a4adf7af0914351d9803fbf01019b506e9efa0
robertkoszegi <robertkoszegi@radiostuntmen.com>
2025-01-06 22:20:28 -0500

    Transform chart and load additional data

--- src/index.js
+++ src/index.js
@@ -1,59 +1,78 @@
 import c3 from 'c3';
 
-const data = [
-	{
-	  "month": "Jan",
-	  "Apples": 327,
-	  "Peaches": 437,
-	  "Pears": 111
-	},
-	{
-	  "month": "Feb",
-	  "Apples": 555,
-	  "Peaches": 454,
-	  "Pears": 222
-	},
-	{
-	  "month": "Mar",
-	  "Apples": 242,
-	  "Peaches": 864,
-	  "Pears": 343
-	},
-	{
-	  "month": "Apr",
-	  "Apples": 222,
-	  "Peaches": 454,
-	  "Pears": 434
-	}
-]
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const options = {
-	bindto: '#chart',
-	axis: {
-		x: { type: 'category' },
-		y: {}
-	},
-	size: {
-		height: 480,
-		width: 960
-	
-	},
-	data: {
-		labels: true,
-		type: "bar",
-		json: data,
-		keys: {
-			x: 'month',
-			value: ['Apples', 'Peaches', 'Pears']
+window.loadChart = function(json) {
+	console.log("loadChart", json);
+	const obj = JSON.parse(json);
+	// console.log(obj);
+	const data = obj.data;
+	const chartType = obj.chartType;
+	// console.log(chartType);
+
+	const options = {
+		bindto: '#chart',
+		axis: {
+			x: { type: 'category' },
+			y: {}
+		},
+		size: {
+			height: 480,
+			width: 960
+		
+		},
+		data: {
+			onclick: function (d) {
+				// console.log("data", d);
+
+				// const index = d.index;
+				// const name = d.name;
+				// const value = d.value;
+				const {index, name, value} = d;
+				
+				const month = months[index]
+				const newObj = {month, name, value};
+				// console.log("newObj", newObj);
+				
+				FileMaker.PerformScript("ChartClick", JSON.stringify(newObj));
+			},
+			labels: true,
+			type: chartType,
+			json: data,
+			keys: {
+				x: 'month',
+				value: ['Apples', 'Peaches', 'Pears']
+			}
+			
+		},
+
+		padding: {
+			bottom: 0
+		
 		}
 		
-	},
-
-	padding: {
-		bottom: 0
-	
 	}
-	
+
+	const chart = c3.generate(options);
+
+	window.transformChart = function(type) {
+		chart.transform(type);
+	}
+
+	window.loadData = function(json) {
+		const obj = JSON.parse(json);
+		console.log(obj);
+		const data = obj.data;
+		console.log(data);
+		chart.load({
+			json: data,
+			keys: {
+				x: 'month',
+				value: ['Bananas'],
+			}
+		})
+	}
+
 }
 
-const chart = c3.generate(options);
+
